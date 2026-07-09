480ad909bb90887eba57480dbf6004203e129cc1
Giovanna Souza <gmidonz09@gmail.com>
2026-07-09 16:24:05 -0300

    Delete Adapter

---
+++ hexagonal/src/main/java/com/cap/hexagonal/adapteres/out/DeleteCustomerByIdAdapter.java
@@ -0,0 +1,18 @@
+package com.cap.hexagonal.adapteres.out;
+
+import com.cap.hexagonal.adapteres.out.repository.CustomerRepository;
+import com.cap.hexagonal.application.ports.out.DeleteCustomerByIdOutputPort;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DeleteCustomerByIdAdapter implements DeleteCustomerByIdOutputPort {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Override
+    public void delete(String id) {
+        customerRepository.deleteById(id);
+    }
+}
